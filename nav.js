408d6d3cf5f083ac3006f867473fd493d5af8bd8
dominikocsofszki <10422001@student.vgu.edu.vn>
2023-10-07 16:21:37 +0200

    $1

--- nav.js
+++ nav.js
@@ -1,19 +1,26 @@
 const nav= `
-    <a href="/" class="here"><img width=100 src="/img/hbrs_logo.png" alt="Home"/></a>
-    <a href="Aufgabe_1/Aufgabe_1.html" >Aufgabe_1</a>      
-    <a href="/kontake.html" >Kontake</a>          
-    <a href="/noten.html" >Noten</a>    
-    <a href="/zeit.html" >Zeit</a>
+<a href="/" class="here"><img width=100 src="/img/hbrs_logo.png" alt="Home"/></a>
+<a href="Aufgabe_1/Aufgabe_1.html" >Aufgabe_1</a>      
+<a href="/kontake.html" >Kontake</a>          
+<a href="/noten.html" >Noten</a>    
+<a href="/zeit.html" >Zeit</a>
 ` ;
+const foot= `
+    <mine id="footer"> <footer>
+    <p>© 2023 Dominik Ocsofszki</p>
+    </footer></mine>
+` ;
+window.addEventListener('DOMContentLoaded', () => {
+    let barnav = document.querySelector('nav[role="navigation"]');
+    barnav.innerHTML = nav;
+});  //(*1)
 
-    window.addEventListener('DOMContentLoaded', () => {
-      let barnav = document.querySelector('nav[role="navigation"]');
-      barnav.innerHTML = nav;
-    });  //(*1)
 
 
-
-
+window.addEventListener('DOMContentLoaded', () => {
+    let barnav = document.querySelector('mine[role="foot"]');
+    barnav.innerHTML = foot;
+});  //(*1)
 
 
 
