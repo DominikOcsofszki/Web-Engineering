a9bd66c171fd60686ead989349ea146626e63c15
dominikocsofszki <10422001@student.vgu.edu.vn>
2023-10-08 15:45:02 +0200

    implemented generate functions that produces navigation, headline, footer

--- js/func.js
+++ js/func.js
@@ -1,7 +1,18 @@
 
+
+const generate=`
+		<mine role="nav"></mine>
+		<mine role="headline"></mine>
+		<mine role="generate"></mine>
+
+		<mine role="foot"></mine>
+`
+
+
+
 const nav= `
 <a href="/" class="here"><img width=100 src="/img/hbrs_logo.png" alt="Home"/></a>
-<a href="Aufgabe_1/Aufgabe_1.html" >Aufgabe_1</a>      
+<a href="/Aufgaben/Aufgabe_1/Aufgabe_1.html" >Aufgabe_1</a>      
 <a href="/kontake.html" >Kontake</a>          
 <a href="/noten.html" >Noten</a>    
 <a href="/zeit.html" >Zeit</a>
@@ -9,25 +20,28 @@ const nav= `
 
 const foot= `
 <footer>
-<p>© 2023 Dominik Ocsofszki</p>
+<p><a  href="/index.html"> home </a>
+© 2023 Dominik Ocsofszki</p> 
 </footer>
 ` ;
 
-const headline=`		
-<h1> Einführung in Web Engineering <h1/>
-`;
+const headline=`<h1 style="text-align:center;"> 
+Einführung in Web Engineering 
+<h1/>`;
 
 
 function insertHTML(name,roleName) {
-
     window.addEventListener('DOMContentLoaded', () => {
         let bla = 'mine[role='+roleName+']'
         let barnav = document.querySelector(bla);
         barnav.innerHTML = name;
     }); 
-
+}function insertAll() {
+    insertHTML(generate, "generate")
+    insertHTML(foot, "foot");
+    insertHTML(nav, "nav");
+    insertHTML(headline, "headline");
 }
-insertHTML(foot, "foot");
-insertHTML(nav, "nav");
-insertHTML(headline, "headline");
 
+
+insertAll()
