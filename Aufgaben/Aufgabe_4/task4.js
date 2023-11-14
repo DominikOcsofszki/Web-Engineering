39157d00b9cd72815793e8cd842b779f8caa4901
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-14 22:15:35 +0100

    new try 4

---
+++ Aufgaben/Aufgabe_4/task4.js
@@ -0,0 +1,7 @@
+// task4
+
+let listOfTasks = [["schlafen", "studieren"], ["essen", "studieren"], ["studieren", "prüfen"]];
+
+for (let [first, second] of listOfTasks) {
+    console.log(first, second);
+}
