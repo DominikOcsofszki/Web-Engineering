a5d6c97b9011ab8efd88bd021ca301cdf9bbdb6a
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-14 14:27:21 +0100

    adding ex 4 task1,2,3

---
+++ Aufgaben/Aufgabe_4/func.js
@@ -0,0 +1,20 @@
+function identity(x) {
+    return x;
+}
+
+function identity_function(x) {
+    return identity(x);
+}
+
+function add(x, y) {
+    return x + y;
+}
+function mul (x,y) {
+    return x * y;
+}
+
+function addf(x) {
+    x_id = identity(x)   
+    return x_id + 
+
+}
