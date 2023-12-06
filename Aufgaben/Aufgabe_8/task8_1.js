18a46352c12c8c22ed67628658893c9502b0cf98
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-12-06 10:00:24 +0100

    task8_1_2

--- Aufgaben/Aufgabe_8/task8_1.js
+++ Aufgaben/Aufgabe_8/task8_1.js
@@ -1,27 +1,27 @@
 
-async function getTexts() {
-    const texTAa = await fetch("localhost:8989/A.txt");
+function getTexts() {
+    const textAa = fetch("localhost:8989/A.txt").then((response) => response.text());//.then((text) => { text; });
     // then((response) => response.text()).then((text) => { text; });
-    const texTBb = await fetch("localhost:8989/B.txt");
-    const texTA = await texTAa.text();
-    const texTB = await texTBb.text();
-    // then((response) => response.text()).then((text) => { text; });
-    // console.log(texTA.text());
-    // console.log(texTB.text());
-    // console.log(texTA);
-    // console.log(texTB);
-    const txtA = texTA.split("\n");
-    const txtB = texTB.split("\n");
-    let newTxtA = [];
-    for (let i = 0; i < txtA.length; i++) {
-        newTxtA.push(txtA[i])
-        newTxtA.push(txtB[i])
-        newTxtA.push("\n")
-    }
-    const txttt = newTxtA.join("");
-    console.log(txttt);
-    // const text = texTA.concat(texTB);
-    // console.log(text);
+    const textBb = fetch("localhost:8989/B.txt").then((response) => response.text());//.then((text) => { text; });
+    Promise.all([textAa, textBb]).then((values) => {
+        const txtA = values[0].split("\n");
+        const txtB = values[1].split("\n");
+        console.log(txtA);
+        console.log(txtB);
+        const newTxtA = txtA.concat(txtB);
+        console.log(newTxtA);
+        const newTxt = [];
+        for (let i = 0; i < txtA.length; i++) {
+            newTxt.push(txtA[i])
+            newTxt.push(txtB[i])
+            newTxt.push("\n")
+        }
+        const finalTxt = newTxt.join("");
+
+        console.log(finalTxt);
+        return finalTxt;
+    });
+
 }
 
 getTexts();
