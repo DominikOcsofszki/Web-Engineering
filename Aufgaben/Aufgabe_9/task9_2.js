df46f47f42c050710eeda068a7d26652b152fcae
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-12-19 16:04:34 +0100

    Adding task 9_3

---
+++ Aufgaben/Aufgabe_9/task9_2.js
@@ -0,0 +1,23 @@
+const x1 = document.getElementById("x1");
+const x2 = document.getElementById("x2");
+const x3 = document.getElementById("x3");
+const x4 = document.getElementById("x4");
+const x5 = document.getElementById("x5");
+const x6 = document.getElementById("x6");
+const x7 = document.getElementById("x7");
+const x8 = document.getElementById("x8");
+const x9 = document.getElementById("#x9");
+
+
+
+// x9.style.backgroundColor = "red";
+// x1.setAttribute("onclick", "change(this,true )");
+//
+// function change(x, isXturn) {
+//   const useXorO = isXturn ? "X" : "O";
+//   if (x.innerHTML == "") {
+//     x.innerHTML = useXorO;
+//   } else {
+//     alert("Feld ist schon belegt!");
+//   }
+// }
